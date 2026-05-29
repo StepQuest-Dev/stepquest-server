626656e5ff6dc6c2a169938d560c00239bd5a48a
janlebda <janjakublebda@gmail.com>
2026-05-29 17:38:50 +0200

    wip

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,12 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    });
 
   const config = app.get(ConfigService);
 
